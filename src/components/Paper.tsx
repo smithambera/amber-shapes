ee19b5ad17abd977d0ff8eee2db456db84b171b0
Amber Smith <amber@chromatic.com>
2021-07-22 16:45:25 -0400

    increase border radius

--- src/components/Paper.tsx
+++ src/components/Paper.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default styled.div<Partial<Props>>(({ interactive = false }) => ({
   background: color.lightest,
   boxShadow: '0 1px 3px rgba(0, 0, 0, 0.05)',
-  borderRadius: 5,
+  borderRadius: 8,
   transition: 'all 150ms ease-out',
   transform: 'translate3d(0, 0, 0)',
   overflow: 'auto',
