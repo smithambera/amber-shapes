d19885c4298f410ff1c593c071e5ecd86fd02439
Amber Smith <amber@chromatic.com>
2021-08-10 16:26:54 -0400

    made some style changes

--- src/components/Shapes.tsx
+++ src/components/Shapes.tsx
@@ -44,7 +44,7 @@ export const Square = (props: Props) => (
 );
 
 export const Triangle = (props: Props) => (
-  <Shape viewBox="0 0 96 88" xmlns="http://www.w3.org/2000/svg" color="green" {...props}>
+  <Shape viewBox="0 0 96 88" xmlns="http://www.w3.org/2000/svg" color="teal" {...props}>
     <path d="M50.63 1.785l44.928 81.77A3 3 0 0192.928 88H3.072a3 3 0 01-2.629-4.445l44.929-81.77a3 3 0 015.258 0z" />
   </Shape>
 );
